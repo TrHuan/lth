8a47d1438fdfa800f6e9f17b7a3fde343009d7e8
TrHuan <trhuan177@gmail.com>
2022-10-24 15:32:14 +0700

    Update lth-blocks.php

--- lth-theme/wp-content/themes/lth_theme/blocks/lth-blocks.php
+++ lth-theme/wp-content/themes/lth_theme/blocks/lth-blocks.php
@@ -29,44 +29,81 @@ foreach(glob(BLOCKS_DIR . '/controls/lazyblock-*/*.php') as $file) {
     require_once($file);
 }
 
-function lth_allowed_block_types( $allowed_blocks ) {     
-    return array(
-        'core/columns',
-        'core/freeform', // Classic
-        // 'core/paragraph',
-        // 'woocommerce/attribute-filter',
-        // 'woocommerce/price-filter',
+if ( class_exists( 'WooCommerce' ) ) {
+    function lth_allowed_block_types( $allowed_blocks ) {     
+        return array(
+            'core/columns',
+            'core/freeform', // Classic
+            // 'core/paragraph',
+            // 'woocommerce/attribute-filter',
+            // 'woocommerce/price-filter',
 
-        'lazyblock/lth-section',
-        // 'lazyblock/lth-blocks',
-        'lazyblock/lth-banner',
-        'lazyblock/lth-blogs',
-        'lazyblock/lth-brand',
-        'lazyblock/lth-shopcart',
-        'lazyblock/lth-categories',
-        'lazyblock/lth-categories-product',
-        'lazyblock/lth-classic',
-        'lazyblock/lth-contact',
-        'lazyblock/lth-features',
-        'lazyblock/lth-gallery',
-        'lazyblock/lth-html-blocks',
-        'lazyblock/lth-list',
-        'lazyblock/lth-languages',
-        'lazyblock/lth-logo',
-        'lazyblock/lth-menu',
-        'lazyblock/lth-products',
-        'lazyblock/lth-search',
-        'lazyblock/lth-skins',
-        'lazyblock/lth-slider',
-        // 'lazyblock/lth-tab',
-        'lazyblock/lth-team',
-        'lazyblock/lth-testimonials',
-        'lazyblock/lth-title',
-        'lazyblock/lth-button',
-        'lazyblock/lth-toggle',
-        'lazyblock/lth-video',
+            'lazyblock/lth-section',
+            'lazyblock/lth-banner',
+            // 'lazyblock/lth-blocks',
+            'lazyblock/lth-blogs',
+            'lazyblock/lth-brand',
+            'lazyblock/lth-button',
+            'lazyblock/lth-categories',
+            'lazyblock/lth-classic',
+            'lazyblock/lth-contact',
+            'lazyblock/lth-features',
+            'lazyblock/lth-gallery',
+            'lazyblock/lth-html-blocks',
+            'lazyblock/lth-languages',
+            'lazyblock/lth-list',
+            'lazyblock/lth-logo',
+            'lazyblock/lth-menu',
+            'lazyblock/lth-search',
+            'lazyblock/lth-skins',
+            'lazyblock/lth-slider',
+            // 'lazyblock/lth-tab',
+            'lazyblock/lth-team',
+            'lazyblock/lth-testimonials',
+            'lazyblock/lth-title',
+            'lazyblock/lth-toggle',
+            'lazyblock/lth-video',
+            
+            'lazyblock/lth-shopcart',
+            'lazyblock/lth-categories-product',
+            'lazyblock/lth-products',
 
-        ////////////////////////////////////
+            ////////////////////////////////////
 
-    );  
-} add_action('allowed_block_types', 'lth_allowed_block_types', 11);
+        );  
+    } add_action('allowed_block_types', 'lth_allowed_block_types', 11);
+} else {
+    function lth_allowed_block_types( $allowed_blocks ) {     
+        return array(
+            'core/columns',
+            'core/freeform',
+
+            'lazyblock/lth-section',
+            'lazyblock/lth-banner',
+            'lazyblock/lth-blogs',
+            'lazyblock/lth-brand',
+            'lazyblock/lth-button',
+            'lazyblock/lth-categories',
+            'lazyblock/lth-classic',
+            'lazyblock/lth-contact',
+            'lazyblock/lth-features',
+            'lazyblock/lth-gallery',
+            'lazyblock/lth-html-blocks',
+            'lazyblock/lth-languages',
+            'lazyblock/lth-list',
+            'lazyblock/lth-logo',
+            'lazyblock/lth-menu',
+            'lazyblock/lth-search',
+            'lazyblock/lth-skins',
+            'lazyblock/lth-slider',
+            'lazyblock/lth-team',
+            'lazyblock/lth-testimonials',
+            'lazyblock/lth-title',
+            'lazyblock/lth-toggle',
+            'lazyblock/lth-video',
+
+            ////////////////////////////////////
+
+        );  
+    } add_action('allowed_block_types', 'lth_allowed_block_types', 11);
+}
