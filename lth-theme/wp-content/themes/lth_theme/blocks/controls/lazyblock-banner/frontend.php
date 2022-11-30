4c85e5a9705b468292f0f16ab3d509e97089732b
TrHuan <trhuan177@gmail.com>
2022-11-30 17:37:01 +0700

    up code

--- lth-theme/wp-content/themes/lth_theme/blocks/controls/lazyblock-banner/frontend.php
+++ lth-theme/wp-content/themes/lth_theme/blocks/controls/lazyblock-banner/frontend.php
@@ -16,30 +16,35 @@ if (!function_exists('lth_banner_output_fe')) :
      * @param string $output - block output.
      * @param array  $attributes - block attributes.
      */
-    function lth_banner_output_fe($output, $attributes) {
+    function lth_banner_output_fe($output, $attributes)
+    {
         ob_start();
-        
+
 ?>
-    <artice class="lth-banners <?php echo $attributes['class']; ?>">
-        <div class="module module_banners">
-            <div class="module_content">
-                <div class="content" style="position: relative;">
-                    <div class="content-image">
-                        <a href="<?php echo esc_url( $attributes['image_url'] ); ?>" title="" class="image">
-                            <img src="<?php echo esc_url( $attributes['image']['url'] ); ?>" alt="Image" width="auto" height="auto">
-                        </a>
-                    </div>
-                    <?php if ($attributes['text']) { ?>
-                        <div class="content-text" style="position: absolute; top: <?php echo $attributes['text_top']; ?>; bottom: <?php echo $attributes['text_bottom']; ?>; left: <?php echo $attributes['text_left']; ?>; right: <?php echo $attributes['text_right']; ?>; transform: translate(<?php echo $attributes['text_translate']; ?>);">
-                            <a href="<?php echo esc_url( $attributes['image_url'] ); ?>" title="" class="text">
-                                <?php echo $attributes['text']; ?>
+        <artice class="lth-banners <?php echo $attributes['class']; ?>">
+            <div class="module module_banners">
+                <div class="module_content">
+                    <div class="content" style="position: relative;">
+                        <div class="content-image">
+                            <a href="<?php echo esc_url($attributes['image_url']); ?>" title="" class="image">
+                                <img src="<?php echo esc_url($attributes['image']['url']); ?>" alt="Image" width="auto" height="auto">
+
+                                <?php if ($attributes['text']) { ?>
+                                    <div class="bg-image"></div>
+                                <?php } ?>
                             </a>
                         </div>
-                    <?php } ?>
+                        <?php if ($attributes['text']) { ?>
+                            <div class="content-text" style="position: absolute; top: <?php echo $attributes['text_top']; ?>; bottom: <?php echo $attributes['text_bottom']; ?>; left: <?php echo $attributes['text_left']; ?>; right: <?php echo $attributes['text_right']; ?>; transform: translate(<?php echo $attributes['text_translate']; ?>);">
+                                <a href="<?php echo esc_url($attributes['image_url']); ?>" title="" class="text">
+                                    <?php echo $attributes['text']; ?>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
-        </div>
-    </artice>
+        </artice>
 
 <?php
         return ob_get_clean();
