aad97b9ba8b2d88c191fa0e8cf58ba107c8b2d96
TrHuan <trhuan177@gmail.com>
2023-08-21 16:52:18 +0700

    del code

--- web-lth-backup/20230724/lth/wp-content/themes/lth-theme/inc/breadcrumbs.php
+++
@@ -1,9 +0,0 @@
-<section class="breadcrumbs">
-    <?php require_once(LIBS_DIR . '/breadcrumbs/breadcrumb-image.php'); ?>
-
-    <article class="breadcrumb-content">
-        <div class="container">
-            <?php require_once(LIBS_DIR . '/breadcrumbs/breadcrumb-content.php'); ?>
-        </div>
-    </article>
-</section>
