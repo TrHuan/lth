81976acb6f219cae76228a2ef81ea726e9ba9fd5
TrHuan <trhuan177@gmail.com>
2023-09-14 09:46:54 +0700

    upcode

---
+++ web-lth-backup/20230914/lth/wp-content/themes/lth-theme/template-parts/content-none.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template hiển thị khi thông báo không tìm thấy posts nào
+ * 
+ * @author LTH
+ * @since 2020
+ */
+?>
+
+<div class="not-found">
+    <?php echo wpautop(__('Xin lỗi, không có dữ liệu nào được tìm thấy.')); ?>
+</div>
