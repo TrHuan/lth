5c67e7c0f0586180f4cb91d2ddd3b2dadbe7ee96
TrHuan <trhuan177@gmail.com>
2023-10-26 09:19:00 +0700

    del

--- web-lth-backup/20231024/lth-cv/wp-content/themes/lth-theme/woocommerce/cart/mini-cart-ajax.php
+++
@@ -1,8 +0,0 @@
-<div class="cart-list">
-	<div class="cart-container-list">
-		<div class="cart-close">
-			<i class="far fa-times icon"></i>
-		</div>
-		<?php woocommerce_mini_cart(); ?>
-	</div>
-</div>
