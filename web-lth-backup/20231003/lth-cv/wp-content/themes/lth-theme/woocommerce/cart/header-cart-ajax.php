24e76556bc38898e45cb4a86349c6f5e9fd47a04
TrHuan <trhuan177@gmail.com>
2023-10-26 09:24:59 +0700

    del

--- web-lth-backup/20231003/lth-cv/wp-content/themes/lth-theme/woocommerce/cart/header-cart-ajax.php
+++
@@ -1,4 +0,0 @@
-<a href="<?php echo wc_get_cart_url(); ?>" title="" class="cart-btn">
-    <span class="fas fa-shopping-cart icon-cart" aria-hidden="true"></span>
-    <span class="item-count"><?php echo  $woocommerce->cart->cart_contents_count ?></span>
-</a>
